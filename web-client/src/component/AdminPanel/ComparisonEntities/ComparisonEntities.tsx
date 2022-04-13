3a6364531fb85ff563ef03e3fd5233422923e89e
nikrainev <nikrainev@gmail.com>
2022-04-13 21:23:34 +0300

    feat: add comparison entities page

---
+++ web-client/src/component/AdminPanel/ComparisonEntities/ComparisonEntities.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './ComparisionEntities.module.less';
+
+export const PAGINATION_PAGE_SIZE = 10;
+
+const ComparisonEntities: React.FC = () => {
+
+    return (
+        <div>
+           Entities
+        </div>
+    );
+};
+
+export default React.memo(ComparisonEntities);
--- web-client/src/const/http/WEB_CLIENT_PATHS.ts
+++ web-client/src/const/http/WEB_CLIENT_PATHS.ts
@@ -1,2 +1,3 @@
 export const LOGIN  = '/login';
 export const CATEGORIES = '/categories';
+export const COMPARISON_ENTITIES = '/comparison-entities';
