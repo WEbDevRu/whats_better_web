91a3d0e11e9c0099116894886901b4044382a615
nikrainev <nikrainev@gmail.com>
2022-05-16 22:58:42 +0300

    fix dependency

--- web-client/src/config/apolloClient.ts
+++ web-client/src/config/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { publicConfig } from './publicConfig';
 
 const client = new ApolloClient({
-    uri: publicConfig.appEnv === 'local' ? 'http://localhost:3001/graphql' : 'http://admin.nikrainev.ru/graphql',
+    uri: publicConfig.appEnv === 'local' ? 'http://localhost:3001/graphql' : '/graphql',
     cache: new InMemoryCache(),
 });
 
